b3fd205d513df8440cbdbb39c5ec74c832b2926c
Rodrigo-Vera2210 <donra2210@gmail.com>
2023-09-11 12:09:33 -0500

    first commit

---
+++ src/containers/errors/Errors.jsx
@@ -0,0 +1,10 @@
+import Layout from "../../hocs/layout/Layout";
+
+export default function Error404() {
+    return(
+        <Layout>
+            Error
+        </Layout>
+    )
+    
+}
---
+++ src/containers/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Layout from "../../hocs/layout/Layout";
+import {connect} from "react-redux"
+
+const mapStateToProps=state=>({
+
+})
+
+export default connect(mapStateToProps,{
+
+}) (function Home() {
+    return(
+        <Layout>
+            Home
+        </Layout>
+    )
+})
